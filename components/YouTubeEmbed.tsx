da3d7d19e0dc3b6e3381a2daa246d76947fdc0cc
hakehardware <ops@hakedev.com>
2025-10-14 18:25:55 -0700

    Added YT Component

--- components/YouTubeEmbed.tsx
+++ components/YouTubeEmbed.tsx
@@ -1,6 +1,6 @@
 export function YouTubeEmbed({ videoId }: { videoId: string }) {
   return (
-    <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
+    <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden', marginTop: '1.5rem', marginBottom: '1.5rem' }}>
       <iframe
         style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
         src={`https://www.youtube.com/embed/${videoId}`}
