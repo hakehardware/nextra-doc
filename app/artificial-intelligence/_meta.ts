e44735d10a1a615ac5dcf7b65f8ed2b76eb8db04
hakedev <ops@hakedev.com>
2025-10-20 09:55:15 -0700

    added zluda for amd radeon 780m

--- app/artificial-intelligence/_meta.ts
+++ app/artificial-intelligence/_meta.ts
@@ -1,3 +1,4 @@
 export default {
-  'comfyui-zluda-amd': 'ComfyUI with ZLUDA (AMD 680M)'
+  'comfyui-zluda-amd-680m': 'ComfyUI with ZLUDA (AMD 680M)',
+  'comfyui-zluda-amd-780m': 'ComfyUI with ZLUDA (AMD 780M)'
 }
