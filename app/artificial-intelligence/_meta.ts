d06612a3049e819eeb421627475678510e5243ee
hakedev <ops@hakedev.com>
2025-10-21 20:38:55 -0700

    removed 680m

--- app/artificial-intelligence/_meta.ts
+++ app/artificial-intelligence/_meta.ts
@@ -1,4 +1,3 @@
 export default {
-  'comfyui-zluda-amd-680m': 'ComfyUI with ZLUDA (AMD 680M)',
-  'comfyui-zluda-amd-780m': 'ComfyUI with ZLUDA (AMD 780M)'
+  'comfyui-zluda-amd-windows': 'ComfyUI with ZLUDA for AMD (Windows)'
 }
