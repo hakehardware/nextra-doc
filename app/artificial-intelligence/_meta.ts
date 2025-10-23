02e95f9ead40382d861118b8124623c52ffbc4c3
hakedev <ops@hakedev.com>
2025-10-22 22:04:53 -0700

    added ollama doc

--- app/artificial-intelligence/_meta.ts
+++ app/artificial-intelligence/_meta.ts
@@ -1,3 +1,4 @@
 export default {
-  'comfyui-zluda-amd-windows': 'ComfyUI with ZLUDA for AMD (Windows)'
+  'comfyui-zluda-amd-windows': 'ComfyUI with ZLUDA for AMD (Windows)',
+  'ollama-for-amd-windows': 'Ollama for AMD Integrated GPUs (Windows)'
 }
