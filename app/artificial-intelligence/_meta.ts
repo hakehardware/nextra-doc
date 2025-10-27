ef58724098319dbfe682770a0cc2c32bd7b56d8a
hakedev <ops@hakedev.com>
2025-10-27 15:39:01 -0700

    added comfyui docs

--- app/artificial-intelligence/_meta.ts
+++ app/artificial-intelligence/_meta.ts
@@ -1,4 +1,7 @@
 export default {
   'comfyui-zluda-amd-windows': 'ComfyUI with ZLUDA for AMD (Windows)',
-  'ollama-for-amd-windows': 'Ollama for AMD Integrated GPUs (Windows)'
+  'ollama-for-amd-windows': 'Ollama for AMD Integrated GPUs (Windows)',
+  'comfyui-ubuntu-server-install': "ComfyUI + Ubuntu Server + CUDA 13 + PyTorch 2.9 (NVIDIA)",
+  'comfyui-install-comfy-manager': "ComfyUI Manager",
+  'comfyui-install-system-monitor': "ComfyUI System Monitor"
 }
